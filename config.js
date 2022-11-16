0b7993cf47eac711b36beb1030d7c35b7fb62b1b
Anukoh <118329149+Anukoh@users.noreply.github.com>
2022-11-16 11:57:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -67,8 +67,8 @@ global.ppulsa2 = '08XXXXXXXXXX' // kalau ada kartu lain isi aja
 global.psaweria = 'https://saweria.co/mrw4h1d'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '62XXXXXXXXXXX'
-global.nomorown = '62XXXXXXXXXXX'
+global.nomorbot = '6288238940013'
+global.nomorown = '6288238940013'
 global.namebot = 'WH-MODS-BOT-V1'
 global.nameown = 'WH MODS DEV'
 
